1d44079e9eaf5c968cfc088c9d440b5f872da16c
Tivadar of THORNS <clement.demazure@etu.inp-n7.fr>
2025-03-31 17:45:43 +0200

    TARGET FOUND PREPARING FOR ANIHILATION

---
+++ GoblinLocalizer.sh
@@ -0,0 +1,6 @@
+sudo ls -R ~ | grep -E -i 'gob'
+sudo ls -R ~ | grep -E -i -c 'gob'
+printf "Potential Goblin founds."
+sudo ls -R ~ | grep -E -i 'goblin'
+sudo ls -R ~ | grep -E -i -c 'goblin'
+printf "Actual Goblin founds."
